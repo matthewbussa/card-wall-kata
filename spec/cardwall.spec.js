54054c03bf263d27ff6bb82fb9003ae4ac2d5182
Matthew Bussa <matthewbussa@gmail.com>
2012-09-22 11:55:42 -0400

    end of day

--- spec/cardwall.spec.js
+++ spec/cardwall.spec.js
@@ -1,14 +1,87 @@
+var inbetween = function(a, b, c) {
+  return a >= b && a <= c;
+};
+
 describe("Cardwall", function() {
-
-  describe("Card", function() {
-
-    it("has a name, description and priority", function(){
-      var card = createCard("name", "desc", 1);
-      expect(card.name).toBe("name");
-      expect(card.description).toBe("desc");
-      expect(card.priority).toBe(1); 
+  describe ("person", function(){
+    var person;
+    it("has a name", function(){
+      person = createPerson("name");
+      expect(person.name).toBe("name");
     });
- 
+
+  });
+  describe("Card", function() {
+    var card;
+    
+    beforeEach(function() {
+      card = createCard("name", "desc", 1);
+    });
+
+    it("has a name", function(){
+      expect(card.name).toBe("name");
+    });
+
+    it("has a description", function(){
+      expect(card.description).toBe("desc");
+    });
+
+    it("has a priority", function(){
+      expect(card.priority).toBe(1);
+    });
+
+    it("has a done default of false", function(){
+      expect(card.done).toBe(false)
+    });
+
+    it("has no assignee", function(){
+      expect(card.assignees).toEqual([])
+    });
+
+    it("has a blocked property starts as false",function(){
+      expect(card.blocked).toBe(false);
+    });
+
+    it("has a start date that is null",function(){
+      expect(card.startDate).toBe(null);
+    });
+    
+    it("has an end date that is null",function(){
+      expect(card.endDate).toBe(null);
+    });
+
+
+
+
+
+
+    it("has one assignee", function() {
+      var person = createPerson("name");
+      card.addPerson(person);
+      expect(card.assignees.length).toBe(1);
+    });
+
+    it("has two assignees", function(){
+        var person = createPerson("name");
+        var person2 = createPerson("name2");
+        card.addPerson(person);
+        card.addPerson(person2);
+        expect(card.assignees.length).toBe(2);
+    });
+
+    it("has unique assignees", function(){
+        var person = createPerson("name");
+        var person2 = createPerson("name2");
+        card.addPerson(person);
+        card.addPerson(person2);
+        card.addPerson(person2);
+        expect(card.assignees.length).toBe(2);
+    });
+
+    it("has complexity property", function(){
+      var card = createCard("name","desc",4)
+      expect(card.complexity).toBeDefined();
+    });
 
   });
 
@@ -20,24 +93,28 @@ describe("Cardwall", function() {
     });
 
     it("has no cards when created", function(){
-      expect(wall.cards).toEqual([]);
+      expect(wall.allCards()).toEqual([]);
+    });
+
+    it("can block a card", function(){
+      var cardOne = createCard("MavisCard", "Desc", 7);
+      wall.addCard(cardOne);
+      wall.blockCard(cardOne);
+      expect(cardOne.blocked).toBe(true);
     });
 
     it("has one card", function() {
       var card = createCard("name", "desc");
       wall.addCard(card);
-      expect(wall.cards.length).toBe(1);      
+      expect(wall.allCards().length).toBe(1);      
     });
 
-    // describe("a two card scenario", function() {
-
-    // });
     it("create 2 cards", function(){
       var cardOne = createCard("w00t", "desc1", 1);
       var cardTwo = createCard("Dos", "desc2", 4);
       wall.addCard(cardOne);
       wall.addCard(cardTwo);
-      expect(wall.cards.length).toBe(2); 
+      expect(wall.allCards().length).toBe(2); 
     });
   
     it("orders 2 cards based on priority", function(){
@@ -46,7 +123,7 @@ describe("Cardwall", function() {
       var cardTwo = createCard("Dos", "desc2", 4);
       wall.addCard(cardOne);
       wall.addCard(cardTwo);
-      expect(wall.cards).toEqual([cardTwo, cardOne]);
+      expect(wall.allCards()).toEqual([cardTwo, cardOne]);
     });
 
     it("orders 3 cards based on priority", function(){
@@ -57,9 +134,66 @@ describe("Cardwall", function() {
       wall.addCard(cardOne);
       wall.addCard(cardThree);
       wall.addCard(cardTwo);
-      expect(wall.cards).toEqual([cardThree, cardTwo, cardOne]);
+      expect(wall.allCards()).toEqual([cardThree, cardTwo, cardOne]);
     });
 
+    it("sets a card to done", function() {
+      var cardOne = createCard("w00t", "desc1", 1)
+      wall.addCard(cardOne);
+      wall.completeCard(cardOne);
+      expect(cardOne.done).toBe(true);
+    });
+
+
+    it("can filter on completeness", function() {
+      var cardOne = createCard("w00t", "desc1", 1);
+      var cardTwo = createCard("Dos", "desc2", 4);
+      var cardThree = createCard("Sherief", "desc3", 6);
+      wall.addCard(cardOne);
+      wall.addCard(cardThree);
+      wall.addCard(cardTwo);
+
+      wall.completeCard(cardTwo);
+
+      expect(wall.completedCards()).toEqual([cardTwo]);
+    });
+
+    it("can filter on completeness part 2", function() {
+      var cardOne = createCard("w00t", "desc1", 1);
+      var cardTwo = createCard("Dos", "desc2", 4);
+      var cardThree = createCard("Sherief", "desc3", 6);
+      wall.addCard(cardOne);
+      wall.addCard(cardThree);
+      wall.addCard(cardTwo);
+
+      wall.completeCard(cardTwo);
+      wall.completeCard(cardThree);
+
+      expect(wall.completedCards()).toEqual([cardThree, cardTwo]);
+    });
+
+    it("can filter on blockedness", function() {
+      var cardOne = createCard("w00t", "desc1", 1);
+      var cardTwo = createCard("Dos", "desc2", 4);
+      var cardThree = createCard("Sherief", "desc3", 6);
+      wall.addCard(cardOne);
+      wall.addCard(cardThree);
+      wall.addCard(cardTwo);
+
+      wall.blockCard(cardTwo);
+      wall.blockCard(cardThree);
+
+      expect(wall.blockedCards()).toEqual([cardThree, cardTwo]);
+    });
+
+    it("has a 'start work' function", function(){
+      var before = Date.now();
+      var cardOne = createCard("w00t", "desc1", 1);
+      wall.addCard(cardOne);
+      wall.startWork(cardOne);
+      var after = Date.now();
+      expect(inbetween(cardOne.startDate.getTime(), before, after)).toBe(true);
+    });
 
   });
 
--- src/cardwall.js
+++ src/cardwall.js
@@ -1,9 +1,20 @@
 var createCard = function(name, description, priority) {
-  
   var card = {
     name: name,
     description: description,
-    priority: priority
+    priority: priority,
+    done: false,
+    blocked: false,
+    assignees: [],
+    addPerson: function(person) {
+      if(this.assignees.indexOf(person) === -1){
+        this.assignees.push(person);
+      };
+    },
+    complexity: 2,
+    startDate: null,
+    endDate: null
+
   };
 
   return card;
@@ -14,14 +25,59 @@ var createWall = function() {
 
   var wall = {
     cards: [],
-    addCard: function(card) {
-      this.cards.push(card);
-      this.cards.sort(function(a,b) {
+    addCard: function(card, statusIndex) {
+      statusIndex = statusIndex || 0;
+      var status = this.cards[statusIndex];
+      if (!status) {
+        status = this.cards[statusIndex] = {cards: []};
+      }
+      var cardsForStatus = status.cards;
+      cardsForStatus.push(card);
+      cardsForStatus.sort(function(a,b) {
         return b.priority - a.priority;
        });
-    }
-  };
+    },
 
+    completeCard: function(card){
+      card.done = true;  
+    },
+
+    blockCard: function(card){
+      card.blocked = true;
+    },
+
+    completedCards: function() {
+      return _(this.allCards()).filter(function(card) {
+        return card.done;
+      });
+    },
+
+    blockedCards: function() {
+      return _(this.allCards()).filter(function(card) {
+        return card.blocked;
+    });
+   },
+
+   startWork: function(card){
+      card.startDate = new Date();
+   },
+
+   allCards: function(){
+    var cards = [];
+    _(this.cards).each(function(status) {
+      cards = cards.concat(status.cards);
+    });
+    return cards;
+   }
+};
   return wall;
 
 };
+
+var createPerson = function(name) {
+  var person = {
+    name: name
+  }
+  return person;
+}
+
